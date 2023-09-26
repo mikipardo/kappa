e11e028d0295c3d344c8456360b86d22110b9a9a
Miki Pardo <92552184+mikipardo@users.noreply.github.com>
2023-09-26 18:57:08 +0200

    Create VariablesEstaticas.java

---
+++ VariablesEstaticas.java
@@ -0,0 +1,9 @@
+/*Nota fuera, una variable estatica afecta a la clase no al objeto, es decir que si creas muchos
+hilos e intancias esa vaariable estática y cambias el resultado, afectará a todos los
+obejtos de esa clase
+*/
+private static String variableEjemplo;
+public class unaClase{
+public  unaClase(){
+  
+}
