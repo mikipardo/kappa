1e16e6267473fa25970ba887ffd9a8585f49e359
Miki Pardo <92552184+mikipardo@users.noreply.github.com>
2023-11-14 08:44:51 +0100

    Create Equals_HasCode.java

---
+++ Equals_HasCode.java
@@ -0,0 +1,2 @@
+Si por ejemplo queremos realizar un HashSet (especie de array list que no se repiten los elementos), al obteto en cuestion debemos implementar en sus atributos 
+  equals and hashCode para que el rpograma pueda reconocer que esos dos objetos son iguales y por lo tanto no guardar los repetidos en el Array
